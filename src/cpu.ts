10eb3b0ffbf3b1768344eaee1d49803b523acfb1
Areo-Joe <1047726015@qq.com>
2023-11-10 14:04:22 +0800

    update: add opcode impl, format code; fix join regis pair; fix carry flag judge with certain bit length

--- src/cpu.ts
+++ src/cpu.ts
@@ -7,10 +7,18 @@ class Z80 {
   }
 
   #registers = {
-        a: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, // for computation
-        f: 0, // flag,
-        sp: 0, // stack pointer
-        pc: 0, // program counter
+    // for computation
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    h: 0,
+    l: 0,
+
+    f: 0, // flag,
+    sp: 0, // stack pointer
+    pc: 0, // program counter
   };
 
   #clock = {
@@ -18,7 +26,20 @@ class Z80 {
     total: 0, // time total
   };
 
-  #opMap = [this.NOP, this.LD_BC_d16, this.LD_BCa_A, this.INC_BC, this.INC_B, this.DEC_B];
+  #opMap = [
+    // 1st
+    this.NOP,
+    this.LD_BC_d16,
+    this.LD_BCa_A,
+    this.INC_BC,
+    this.INC_B,
+    this.DEC_B,
+    this.LD_B_d8,
+    this.RLCA,
+
+    this.LD_d16a_SP,
+    this.ADD_HL_BC,
+  ];
 
   run() {
     while (true) {
@@ -26,9 +47,9 @@ class Z80 {
       this.#opMap[opcode]();
     }
   }
-  
+
   reset() {
-    (["register", "clock"] as Array<keyof this>).forEach((resetKey) => {
+    (['register', 'clock'] as Array<keyof this>).forEach((resetKey) => {
       Object.keys(this[resetKey] as Record<string, number>).forEach(
         (key) => ((this[resetKey] as Record<string, number>)[key] = 0)
       );
@@ -43,7 +64,7 @@ class Z80 {
     higherByte: Z80Registers,
     lowerByte: Z80Registers
   ): number {
-    return this.#registers[higherByte] << (8 + this.#registers[lowerByte]);
+    return (this.#registers[higherByte] << 8) + this.#registers[lowerByte];
   }
 
   private distributeToRegisterPair(
@@ -104,6 +125,7 @@ class Z80 {
   // ***** THE FUNCTIONS BELOW ARE OPCODES!!! *****
 
   // ***** [1st 8 ops] [0x00 - 0x07] starts *****
+
   private NOP() {}
 
   private LD_BC_d16() {
@@ -114,13 +136,13 @@ class Z80 {
   }
 
   private LD_BCa_A() {
-    let addr = this.joinRegisterPair("b", "c");
+    let addr = this.joinRegisterPair('b', 'c');
     this.#memory.writeByte(addr, this.#registers.a);
   }
 
   private INC_BC() {
-    let val = this.joinRegisterPair("b", "c");
-    this.distributeToRegisterPair("b", "c", (val + 1) & 0xffff);
+    let val = this.joinRegisterPair('b', 'c');
+    this.distributeToRegisterPair('b', 'c', (val + 1) & 0xffff);
   }
 
   private INC_B() {
@@ -129,7 +151,9 @@ class Z80 {
     this.#registers.b = result;
     this.setZeroFlag(shouldSetZeroFlag(result));
     this.setSubstractionFlag(false);
-    this.setHalfCarryFlag(shouldSetHalfCarryFlag(val, 1, Operation.Add));
+    this.setHalfCarryFlag(
+      shouldSetHalfCarryFlag(val, 1, Operation.Add, BitLength.OneByte)
+    );
   }
 
   private DEC_B() {
@@ -137,7 +161,9 @@ class Z80 {
     let result = (val - 1) & 0xff;
     this.setZeroFlag(shouldSetZeroFlag(result));
     this.setSubstractionFlag(true);
-    this.setHalfCarryFlag(shouldSetHalfCarryFlag(val, 1, Operation.Minus));
+    this.setHalfCarryFlag(
+      shouldSetHalfCarryFlag(val, 1, Operation.Minus, BitLength.OneByte)
+    );
   }
 
   private LD_B_d8() {
@@ -153,6 +179,29 @@ class Z80 {
   }
 
   // ***** [1st 8 ops] [0x00 - 0x07] ends *****
+
+  // ***** [2nd 8 ops] [0x00 - 0x07] starts  *****
+
+  private LD_d16a_SP() {
+    const addrLB = this.readFromPcAndIncPc();
+    const addrHB = this.readFromPcAndIncPc();
+    const addr = ((addrHB << 8) & 0xff) + (0xff & addrLB);
+    this.#memory.writeDoubleByte(addr, this.#registers.sp);
+  }
+
+  private ADD_HL_BC() {
+    const bc = this.joinRegisterPair('b', 'c');
+    const hl = this.joinRegisterPair('h', 'l');
+    const result = (hl + bc) & 0xff;
+    this.distributeToRegisterPair('h', 'l', result);
+    this.setSubstractionFlag(false);
+    this.setHalfCarryFlag(
+      shouldSetHalfCarryFlag(hl, bc, Operation.Add, BitLength.DoubleByte)
+    );
+    this.setCarryFlag(
+      shouldSetCarryFlag(hl, bc, Operation.Add, BitLength.DoubleByte)
+    );
+  }
 }
 
 export abstract class MMU {
@@ -163,11 +212,16 @@ export abstract class MMU {
   abstract writeDoubleByte(addr: number, val: number): void;
 }
 
-type Z80Registers = "a" | "b" | "c" | "d" | "e" | "h" | "l" | "f" | "sp" | "pc";
+type Z80Registers = 'a' | 'b' | 'c' | 'd' | 'e' | 'h' | 'l' | 'f' | 'sp' | 'pc';
 
 enum Operation {
   Add,
   Minus,
+}
+
+enum BitLength {
+  OneByte = 8,
+  DoubleByte = 16,
 }
 
 function shouldSetZeroFlag(result: number) {
@@ -177,11 +231,37 @@ function shouldSetZeroFlag(result: number) {
 function shouldSetHalfCarryFlag(
   left: number,
   right: number,
-  operation: Operation
-) {
+  operation: Operation,
+  bitLength: number
+): boolean {
+  assertEven(bitLength);
+  const halfBitLength = bitLength / 2;
+  return shouldSetCarryFlag(left, right, operation, halfBitLength);
+}
+
+function shouldSetCarryFlag(
+  left: number,
+  right: number,
+  operation: Operation,
+  bitLength: number
+): boolean {
+  const fullOnes = (1 << bitLength) - 1;
+
+  const [leftWithLimitedBits, rightWithLimitedBits] = [left, right].map(
+    (x) => x & fullOnes
+  );
+
   if (operation === Operation.Add) {
-    return (((left & 0xf) + (right & 0xf)) & 0x10) !== 0;
+    return (
+      ((leftWithLimitedBits + rightWithLimitedBits) & (1 << bitLength)) !== 0
+    );
   } else {
-    return (left & 0xf) < (right & 0xf);
+    return leftWithLimitedBits < rightWithLimitedBits;
+  }
+}
+
+function assertEven(x: number) {
+  if (x % 2 !== 0) {
+    throw new Error(`${x} is not even!`);
   }
 }
