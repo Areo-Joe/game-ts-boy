e2b9f6b554bb8071ed04fd707626b86b7a14910f
Areo-Joe <1047726015@qq.com>
2024-03-27 10:47:03 +0800

    fix debounced main script handler

--- src/runner/web/mainScript.ts
+++ src/runner/web/mainScript.ts
@@ -1,4 +1,5 @@
 import ROM from '../../../rom/opus5.gb';
+import { debounce } from '../../utils';
 
 const canvas = document.getElementById('canvas')! as HTMLCanvasElement;
 const context = canvas.getContext('2d');
@@ -13,22 +14,8 @@ const handleMessage = (e: any) => {
   const imgData = new ImageData(clamped, 160, 144);
   context.putImageData(imgData, 0, 0);
 };
-const saveStream = <T>(fn: (...props: T[]) => unknown, time: number) => {
-  let canCall = true;
-  return (...props: T[]) => {
-    if (canCall) {
-      fn(...props);
-      canCall = false;
-      setTimeout(() => {
-        canCall = true;
-      }, time);
-    }
-  };
-};
-
-const savedHandle = saveStream(handleMessage, 300);
-
-worker.onmessage = savedHandle;
+const debouncedHandle = debounce(handleMessage, 300);
+worker.onmessage = debouncedHandle;
 fetch(ROM)
   .then((res) => res.arrayBuffer())
   .then((buffer) => {
